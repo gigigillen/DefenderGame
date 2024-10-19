51aa8fae3460a5b200d563e13b0d0b2813bb560c
ec22955 <annabel.s@me.com>
2024-10-19 13:07:49 +0100

    apprentice counter and limit

--- Assets/Scripts/SpawnApprentice.cs
+++ Assets/Scripts/SpawnApprentice.cs
@@ -2,41 +2,52 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.UI;
+using TMPro;
 
-public class SpawnApprentice : MonoBehaviour
-{
+public class SpawnApprentice : MonoBehaviour { 
+
     public GameObject apprentice; // The prefab to instantiate
     private Camera cam;
 
+    private int apprenticeCount;
+    private const int maxApprentices = 5; // max apprentices is immutable
+    public TextMeshProUGUI apprenticeText;
+
     // Start is called before the first frame update
-    void Start()
-    {
-        Debug.Log("SCRIPT RUN");
+    void Start() {
+
         cam = Camera.main;
+        apprenticeCount = 0;
+        apprenticeText.text = "";
+        SetApprenticeText();
     }
 
     // Update is called once per frame
-    void FixedUpdate()
-    {
+    void FixedUpdate() {
 
+        SetApprenticeText();
     }
 
-    void OnSpawn()
-    {
-        if (Mouse.current.leftButton.wasPressedThisFrame)
-        {
+    void OnSpawn() { 
+    
+        if (apprenticeCount < maxApprentices && Mouse.current.leftButton.wasPressedThisFrame) { 
+        
             Ray ray = cam.ScreenPointToRay(Mouse.current.position.ReadValue());
             RaycastHit hit;
-            Debug.Log("MOUSE CLICK");
 
-            if (Physics.Raycast(ray, out hit))
-            {
+            if (Physics.Raycast(ray, out hit)) { 
+            
                 Vector3 spawnPosition = new Vector3(hit.point.x, 0.5f, hit.point.z);
 
                 // Instantiate the apprentice prefab at the hit point
                 Instantiate(apprentice, spawnPosition, Quaternion.identity);
-                Debug.Log("INSTANTIATE");
+                apprenticeCount++;
             }
         }
+    }
+
+    private void SetApprenticeText() {
+        apprenticeText.text = "Apprentices: " + apprenticeCount.ToString() + "/" + maxApprentices;
     }
 }
