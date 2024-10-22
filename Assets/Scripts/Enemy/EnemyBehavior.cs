abcb569895bf8e888b4831068c3ba0d8372a08bd
Gigi Gillen <117616034+gigigillen@users.noreply.github.com>
2024-10-22 16:53:54 +0100

    Update comments EnemyBehavior.cs

--- Assets/Scripts/Enemy/EnemyBehavior.cs
+++ Assets/Scripts/Enemy/EnemyBehavior.cs
@@ -7,21 +7,23 @@ public class EnemyBehavior : MonoBehaviour
 
     public GameObject enemy;
     public GameObject stronghold;
+
+    // enemy speed
     public float speed;
 
 
-    // Start is called before the first frame update
     void Start()
     {
 
     }
 
-    // Update is called once per frame
+    // enemies uniform attack to the stronghold
     void FixedUpdate()
     {
-        // Reset the Y position of the stronghold
+        // where the enemies should be charging towards
         Vector3 targetPosition = stronghold.transform.position;
-        targetPosition.y = enemy.transform.position.y; // Set Y to the enemy's Y position or a fixed value
+        // ensures the enemies attack on a fixed y position
+        targetPosition.y = enemy.transform.position.y;
 
         enemy.transform.position = Vector3.MoveTowards(enemy.transform.position, targetPosition, speed);
 
