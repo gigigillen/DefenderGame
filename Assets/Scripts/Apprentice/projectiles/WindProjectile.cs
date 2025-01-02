18748610215344852ad5fa4b533065282193a30e
ec22955 <annabel.s@me.com>
2025-01-02 16:05:57 +0000

    wind vortex added

--- Assets/Scripts/Apprentice/projectiles/WindProjectile.cs
+++ Assets/Scripts/Apprentice/projectiles/WindProjectile.cs
@@ -3,8 +3,39 @@ using System.Collections;
 
 public class WindProjectile : ProjectileController {
 
+    // unlocked further in the skill tree
+    private bool canCreateVortex = true;
+    [SerializeField] private GameObject vfxVortexPrefab;
 
     private void Awake() {
+
         projectileType = ApprenticeType.Wind;
+    }
+
+    // call this when vortex skill is unlocked
+    public void EnableVortex() {
+
+        canCreateVortex = true;
+    }
+
+    protected override void OnReachTarget() {
+
+        DealDamage();
+
+        if (canCreateVortex && owner != null) {
+
+            if (VaporiseController.CanVaporise(target.gameObject) && VortexController.CanCreateVortex(owner)) {
+                Vector3 impactPosition = transform.position;
+                impactPosition.y = 0.1f;
+                GameObject vortexObject = Instantiate(vfxVortexPrefab, impactPosition, Quaternion.identity);
+                VortexEffect vortexEffect = vortexObject.GetComponent<VortexEffect>();
+                if (vortexEffect != null) {
+                    vortexEffect.StartVortex();
+                    VortexController.RecordVortex(owner);
+                }
+            }
+        }
+
+        pool.ReturnProjectile(gameObject);
     }
 }
